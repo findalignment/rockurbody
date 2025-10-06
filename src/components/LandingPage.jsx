dd46192a979db0eed2d888dfe6aefbdd39a32501
Rock Hudson <rockhudson@Rocks-MacBook-Pro.local>
2025-10-05 20:05:43 -0700

    Move chat bar to bottom of viewport (95-100vh) for maximum hero image visibility

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -214,7 +214,7 @@ function LandingPage() {
 
   return (
     <Hero>
-      <div className="w-full flex flex-col items-center justify-end space-y-4 pb-12 pt-[85vh] md:pt-[90vh]">
+      <div className="w-full flex flex-col items-center justify-end space-y-4 pb-8 pt-[95vh] md:pt-[100vh]">
         <HeroContent />
         
         <div className="w-full max-w-4xl mx-auto px-4">
