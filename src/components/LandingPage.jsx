488eba9402e02227bdce36a1b8bb998143ff925c
Rock Hudson <rockhudson@Rocks-MacBook-Pro.local>
2025-12-15 15:58:24 -0800

    Update main page hero to use hero-background.jpg

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -339,7 +339,7 @@ function LandingPage() {
       structuredData={getLocalBusinessSchema()}
       url="/"
     />
-    <Hero>
+    <Hero backgroundImage="/hero-background.jpg" altText="Rock Your Body - Structural integration and movement education in Santa Cruz, California">
       <div className="w-full flex flex-col items-center justify-end space-y-4 pb-8 pt-[85vh] md:pt-[90vh]">
         <HeroContent />
         
